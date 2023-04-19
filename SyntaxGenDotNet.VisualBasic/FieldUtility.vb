70119e622893e50c7a2596d2d01a528ce5e41eed
Oliver Booth <me@olivr.me>
2023-04-20 00:22:31 +0100

    docs: add xmldoc for VB FieldUtility methods

--- SyntaxGenDotNet.VisualBasic/FieldUtility.vb
+++ SyntaxGenDotNet.VisualBasic/FieldUtility.vb
@@ -2,12 +2,22 @@
 Imports SyntaxGenDotNet.Syntax
 
 Friend Module FieldUtility
+    ''' <summary>
+    '''     Writes the modifiers for a field declaration.
+    ''' </summary>
+    ''' <param name="declaration">The declaration to write to.</param>
+    ''' <param name="fieldInfo">The field whose modifiers to write.</param>
     Public Sub WriteModifiers(declaration As SyntaxNode, fieldInfo As FieldInfo)
         If fieldInfo.IsLiteral Then
             declaration.AddChild(ConstKeyword)
         End If
     End Sub
 
+    ''' <summary>
+    '''     Writes the visibility keyword for a field declaration.
+    ''' </summary>
+    ''' <param name="declaration">The declaration to write to.</param>
+    ''' <param name="fieldInfo">The field whose visibility to write.</param>
     Public Sub WriteVisibilityKeyword(declaration As SyntaxNode, fieldInfo As FieldInfo)
         Const mask = FieldAttributes.FieldAccessMask
 
