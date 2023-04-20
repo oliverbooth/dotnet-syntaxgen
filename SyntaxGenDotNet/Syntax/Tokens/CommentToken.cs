a0afd2c68033fd075c36661b55235e92def56803
Oliver Booth <me@olivr.me>
2023-04-20 01:09:08 +0100

    fix(feat): add missing CommentToken

---
+++ SyntaxGenDotNet/Syntax/Tokens/CommentToken.cs
@@ -0,0 +1,20 @@
+﻿namespace SyntaxGenDotNet.Syntax.Tokens;
+
+/// <summary>
+///     Represents a comment token.
+/// </summary>
+public class CommentToken : SyntaxToken
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="CommentToken" /> class.
+    /// </summary>
+    /// <param name="comment">The comment.</param>
+    public CommentToken(string? comment)
+    {
+        Text = $"// {comment}";
+        LeadingWhitespace = WhitespaceTrivia.Space;
+    }
+
+    /// <inheritdoc />
+    public override string Text { get; }
+}
