47deb98063930856f5e34a8bd15c9411aa3d93ba
Oliver Booth <me@olivr.me>
2023-04-21 16:03:15 +0100

    refactor: provide default impl for enum/delegate generation

--- SyntaxGenDotNet/SyntaxGenerator.cs
+++ SyntaxGenDotNet/SyntaxGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+using System.Diagnostics;
+using System.Reflection;
+using SyntaxGenDotNet.Attributes;
 using SyntaxGenDotNet.Syntax.Declaration;
 
 namespace SyntaxGenDotNet;
@@ -31,18 +33,54 @@ public abstract class SyntaxGenerator
     public string LanguageName { get; protected set; } = string.Empty;
 
     /// <summary>
+    ///     Generates the syntax for the specified member.
+    /// </summary>
+    /// <param name="member">The member for which to generate syntax.</param>
+    /// <returns>The syntax for the specified member.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="member" /> is <see langword="null" />.</exception>
+    public DeclarationSyntax GenerateDeclaration(MemberInfo member)
+    {
+        if (member is null)
+        {
+            throw new ArgumentNullException(nameof(member));
+        }
+
+        return member switch
+        {
+            Type type when type.IsSubclassOf(typeof(MulticastDelegate)) || type.IsSubclassOf(typeof(Delegate)) =>
+                GenerateDelegateDeclaration(type),
+            Type {IsEnum: true} type => GenerateEnumDeclaration(type),
+            Type type => GenerateTypeDeclaration(type),
+            FieldInfo fieldInfo => GenerateFieldDeclaration(fieldInfo),
+            MethodInfo methodInfo => GenerateMethodDeclaration(methodInfo),
+            // PropertyInfo propertyInfo => GeneratePropertyDeclaration(propertyInfo),
+            // ConstructorInfo constructorInfo => GenerateConstructorDeclaration(constructorInfo),
+            // EventInfo eventInfo => GenerateEventDeclaration(eventInfo),
+            _ => throw new NotSupportedException()
+        };
+    }
+
+    /// <summary>
     ///     Generates the syntax for the specified enum.
     /// </summary>
     /// <param name="enumType">The enum for which to generate syntax.</param>
     /// <returns>The syntax for the specified enum.</returns>
-    public abstract TypeDeclaration GenerateEnumDeclaration(Type enumType);
+    public virtual TypeDeclaration GenerateEnumDeclaration(Type enumType)
+    {
+        Trace.Assert(enumType.IsEnum);
+        return GenerateTypeDeclaration(enumType);
+    }
 
     /// <summary>
     ///     Generates the syntax for the specified delegate.
     /// </summary>
     /// <param name="delegateType">The delegate for which to generate syntax.</param>
     /// <returns>The syntax for the specified delegate.</returns>
-    public abstract TypeDeclaration GenerateDelegateDeclaration(Type delegateType);
+    public virtual TypeDeclaration GenerateDelegateDeclaration(Type delegateType)
+    {
+        Trace.Assert(delegateType.IsSubclassOf(typeof(MulticastDelegate)) || delegateType.IsSubclassOf(typeof(Delegate)));
+        return GenerateTypeDeclaration(delegateType);
+    }
 
     /// <summary>
     ///     Generates the syntax for the specified field.
