6969c850749ee7a002155c06b875dda76444bd80
Oliver Booth <me@olivr.me>
2023-05-01 17:48:09 +0100

    feat(vb): add ctor generation

--- SyntaxGenDotNet.VisualBasic/VisualBasicSyntaxGenerator.Constructor.vb
+++ SyntaxGenDotNet.VisualBasic/VisualBasicSyntaxGenerator.Constructor.vb
@@ -1,9 +1,23 @@
 ﻿Imports System.Reflection
 Imports SyntaxGenDotNet.Syntax.Declaration
+Imports SyntaxGenDotNet.VisualBasic.Utilities
 
 Public Partial Class VisualBasicSyntaxGenerator
     ''' <inheritdoc/>
     Public Overrides Function GenerateConstructorDeclaration(constructorInfo As ConstructorInfo) As ConstructorDeclaration
-        Return New ConstructorDeclaration()
+        Dim declaration As New ConstructorDeclaration()
+
+        WriteVisibilityModifier(declaration, constructorInfo)
+        If constructorInfo.IsStatic Then
+            declaration.AddChild(SharedKeyword)
+        End If
+
+        declaration.AddChild(SubKeyword)
+        declaration.AddChild(NewKeyword)
+        declaration.AddChild(OpenParenthesis)
+        WriteParameters(declaration, constructorInfo.GetParameters())
+        declaration.AddChild(CloseParenthesis)
+
+        Return declaration
     End Function
 End Class
